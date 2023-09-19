97faaeedc73d8922090057b66d9c7a5a93df5266
crucial-sub <jssub940@gmail.com>
2023-09-19 23:59:11 +0900

    project init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  useTabs: false,
+  bracketSpacing: true,
+};
