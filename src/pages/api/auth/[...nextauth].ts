f1dd72fd025def4af4876a2d3e0619aebc4433a1
crucial-sub <jssub940@gmail.com>
2023-09-20 03:34:31 +0900

    style: next-auth 파일 주석 수정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,29 +4,28 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 export default NextAuth({
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: '이메일', type: 'type', placeholder: 'jsmith' },
-        password: { label: '비밀번호', type: 'password' },
+        email: {
+          label: '이메일',
+          type: 'text',
+          placeholder: '이메일을 입력하세요.',
+        },
+        password: {
+          label: '비밀번호',
+          type: 'password',
+          placeholder: '비밀번호를 입력하세요',
+        },
       },
+
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
+        // 추후 db 유효성 검사 로직 추가
         const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
 
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
           return user as any;
         } else {
-          // If you return null or false then the credentials will be rejected
           return null;
-          // You can also Reject this callback with an Error or with a URL:
-          // throw new Error("error message") // Redirect to error page
-          // throw "/path/to/redirect"        // Redirect to a URL
         }
       },
     }),
