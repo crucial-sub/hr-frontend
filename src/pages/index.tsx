80d64cfa94aeb33f29949efb26ea9e70c296cb5c
crucial-sub <jssub940@gmail.com>
2023-09-20 06:02:43 +0900

    feat: 메인 페이지에서 로그인 여부를 getServerSideProps를 통해 확인하는 방식으로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
+import { GetServerSidePropsContext } from 'next';
+import { getServerSession } from 'next-auth';
 import { signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { authOptions } from './api/auth/[...nextauth]';
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  if (status === 'loading') return <></>;
-  else if (status === 'unauthenticated') {
-    router.push('/api/auth/signin');
-  } else if (status === 'authenticated') {
-    return (
-      <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24`}
-      >
-        <p>로그인 이메일: {session.user?.email}</p>
-        <button onClick={() => signOut()}>로그아웃</button>
-      </main>
-    );
-  }
+  const { data: session } = useSession();
+  if (!session) return <></>;
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+      <p>로그인 이메일: {session.user?.email}</p>
+      <button onClick={() => signOut()}>로그아웃</button>
+    </main>
+  );
 }
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+      },
+    };
+  }
+
+  return { props: {} };
+};
