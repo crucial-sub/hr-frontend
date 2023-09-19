be0cd53ff7a5fde5cd57c5a46a8763c5f39cda3d
crucial-sub <jssub940@gmail.com>
2023-09-20 03:54:29 +0900

    feat: useSession을 이용하여 로그인 여부 확인 & 로그아웃 버튼 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,16 @@
+import { signOut, useSession } from 'next-auth/react';
+
 export default function Home() {
+  const { data: session, status } = useSession();
+  if (status === 'authenticated') {
+    return (
+      <div>
+        <p>로그인 이메일: {session.user?.email}</p>
+        <button onClick={() => signOut()}>로그아웃</button>
+      </div>
+    );
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
